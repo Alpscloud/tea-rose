4c512dfbfcb9513c20b6f6a4465db5ecad03925f
winde <winde100kg@gmail.com>
2020-05-23 12:28:32 +0300

    added header

--- src/js/common.js
+++ src/js/common.js
@@ -17,5 +17,54 @@ $(document).ready(function() {
 		}, 200);
 	});
 
+	// Fancybox
+	$('[data-fancybox]').fancybox({
+		loop: true,
+		buttons: ['close']
+	});
+
+	// quantity
+	$('.product__quantity').on('click', function(event) {
+		var input = $(this).find('.js-quantity-input'),
+			value = input.val(),
+			target = $(event.target);
+		
+		if(target.attr('data-action') === 'plus') {
+			value++;
+			
+			input.val(value);
+		
+
+		} else if(target.attr('data-action') === 'minus') {	
+			if(input.val() <= 1) {return};
+			value--;	
+			
+			input.val(value);
+			
+		}
+
+	});
+
+	// Sliders
+	var promoSlider = new Swiper('.js-promo-slider', {
+		speed: 600,
+		spaceBetween: 0,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false
+		},
+		pagination: {
+			el: '.js-promo-slider-pagination',
+			clickable: true
+		},
+		navigation: {
+			nextEl: '.js-promo-slider-btn-next',
+			prevEl: '.js-promo-slider-btn-prev',
+		},
+	});
+
+
+	$('input[type=tel]').mask('+38 (999) 999-99-99');
+
 
 });
